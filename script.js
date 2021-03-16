1843e726b6e7b803cb19098431e776558ca7fe6a
SophieTopart <sophiebailly9@gmail.com>
2021-03-16 18:15:42 +0100

    1er jet header HTML, CSS, JS

---
+++ script.js
@@ -0,0 +1,24 @@
+const Sophie = document.querySelector("#Sophie")
+const Aline = document.querySelector("#Aline")
+const Kevin = document.querySelector("#Kevin")
+const Gregory = document.querySelector("#Gregory")
+
+Sophie.onclick = function (){
+    document.location.href="Sophie.html"
+}
+
+Aline.onclick = function (){
+    document.location.href="Aline.html"
+}
+
+Kevin.onclick = function (){
+    document.location.href="Kevin.html"
+}
+
+Gregory.onclick = function (){
+    document.location.href="Gregory.html"
+}
+
+Sophie.addEventListener("mouseover", function(){
+
+})
