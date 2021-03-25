90287a2af062710b87d9af73501f2dbd712dacac
Aline Dubois <juillet.aline@gmail.com>
2021-03-25 08:50:44 +0100

    intégration du dark mode et de la popup cv

--- script-aline.js
+++ script-aline.js
@@ -6,4 +6,31 @@ boutonCta.addEventListener("click", () => {
 })
 
 
+/* Animation CV */
+const modal = document.getElementById("myModal");
+const spanCV = document.getElementById("logoPlus");
+const spanClose = document.getElementsByClassName("close")[0];
+
+spanCV.onclick = function() {
+    modal.style.display = "block";
+}
+
+spanClose.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if(event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+/* Dark mode */
+function darkMode() {
+    const element = document.body;
+    element.classList.toggle("dark-mode");
+    const modalContent = document.getElementById("modalContent");
+    modalContent.classList.toggle("dark-mode-modal");
+}
+
 
